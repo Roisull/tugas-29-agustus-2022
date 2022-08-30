2a4c04c72e9d8e70fe529abb6964bc548d799184
lusiro <lusiro2001@students.amikom.ac.id>
2022-08-30 13:05:01 +0700

    update tugas 29/08/2022 roisul

---
+++ app/src/main/java/com/example/chapter3top1/NilaiMhsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.chapter3top1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NilaiMhsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nilai_mhs)
+    }
+}
